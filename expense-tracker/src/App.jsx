8e15aa71e0314dcc043525a5bf5481225f775f96
kotzimop <kotzimop@gmail.com>
2023-04-30 08:25:15 +0300

    Use custom css for a component

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <>
       <div>
         <p>Let's get started</p>
+        <ExpenseItem />
       </div>
     </>
   );
