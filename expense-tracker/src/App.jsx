ba3821d6f4290284e13206b10b5c7a2e3ecc76fe
kotzimop <kotzimop@gmail.com>
2023-04-30 08:06:07 +0300

    Initial commit

---
+++ expense-tracker/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Let's get started</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
