2edca29d039120e76733a6c8edc45a64fb0944ec
kotzimop <kotzimop@gmail.com>
2023-04-30 08:45:03 +0300

    Pass props to app

--- expense-tracker/src/components/ExpenseItem.jsx
+++ expense-tracker/src/components/ExpenseItem.jsx
@@ -1,16 +1,17 @@
-import './ExpenseItem.css'
+/* eslint-disable react/prop-types */
 
-const ExpenseItem = () => {
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
-        <div>March 28th 2021</div>
-        <div className='expense-item__decription'>
-            <h2>Car Insurance</h2>
-            <div className='expense-item__price'>$294.67</div>
-        </div>
-
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
