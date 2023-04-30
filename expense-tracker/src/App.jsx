54ab4cdda26f38581f6036a840d793312076ad72
kotzimop <kotzimop@gmail.com>
2023-04-30 10:02:13 +0300

    Dynamic class & Refactor: Add new component

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -28,25 +28,7 @@ function App() {
     <>
       <h2>Let's get started</h2>
       <div>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-      </div>
-      <div>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-      </div>
-      <div>
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
+        <Expenses items={expenses} />
       </div>
     </>
   );
---
+++ expense-tracker/src/components/Card.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import "./Card.css";
+
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>;
+};
+
+export default Card;
