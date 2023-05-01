be6ce88a485a17995534ddc0f592569d08be5693
kotzimop <kotzimop@gmail.com>
2023-05-01 10:33:13 +0300

    2-way binding in Form

--- expense-tracker/src/components/NewExpense/ExpenseForm.jsx
+++ expense-tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -7,32 +7,31 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAMount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
 
-
   const titleChangeHandler = e => {
     setEnteredTitle(e.target.value);
-
   };
 
   const amountChangeHandler = e => {
     setEnteredAMount(e.target.value);
-
   };
 
   const dateChangeHandler = e => {
     setEnteredDate(e.target.value);
-
   };
 
   const submitHandler = event => {
     event.preventDefault();
-    
-    const expenseData = {
-        title: enteredTitle,
-        amount: enteredAmount,
-        date: new Date(enteredDate)
-    }
 
-    console.log(expenseData)
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAMount('')
+    setEnteredDate('')
   };
 
   return (
@@ -40,12 +39,17 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -55,6 +59,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
