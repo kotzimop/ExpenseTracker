ebe81364558e33fc5b6d77a4929a1bc98c18a31f
kotzimop <kotzimop@gmail.com>
2023-05-04 11:13:51 +0300

    Split Expenses create ExpensesList component

--- expense-tracker/src/components/Expenses/Expenses.jsx
+++ expense-tracker/src/components/Expenses/Expenses.jsx
@@ -5,30 +5,24 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 const Expenses = props => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const handleYear = year => {
-    setFilteredYear(year)
-
+    setFilteredYear(year);
   };
 
   const filteredExpenses = props.items.filter(expense => {
-    return expense.date.getFullYear().toString() === filteredYear 
-  })
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
       <ExpenseFilter selected={filteredYear} onSelectedYear={handleYear} />
-      {filteredExpenses.map(item => (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />
-      ))}
+      <ExpensesList items={filteredExpenses}/>
+
     </Card>
   );
 };
